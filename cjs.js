26413316f218e9be20bf5c039866a4d9a796c755
Shen Lang <shenlang@sjtu.edu.cn>
2021-08-18 14:40:32 +0800

    feat(global): if there already exist require, do nothing but log the situation

--- cjs.js
+++ cjs.js
@@ -20,6 +20,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 (function(){
+    if(globalThis.require) {
+        log('require already exist');
+    }
     log('CJS started');
 
     let coreModules = new Map([]);
